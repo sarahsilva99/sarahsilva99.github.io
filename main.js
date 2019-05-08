69c1ebbc152fa62ac7a8c005f8294d77795ea663
Sarah Silva <46930796+sarahsilva99@users.noreply.github.com>
2019-05-08 11:26:38 -0400

    final

---
+++ main.js
@@ -0,0 +1,14 @@
+(function(){
+    var words = [
+        'a Designer',
+        'a Creator',
+        'an Artist',
+        
+        ], i = 0;
+    setInterval(function(){
+        $('#changingword').fadeOut(function(){
+            $(this).html(words[i=(i+1)%words.length]).fadeIn();
+        });
+    }, 3000);
+      
+})();
